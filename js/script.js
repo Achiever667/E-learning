b760b725e58caef49e5f5de8d7d205c5112bfc21
achiever667 <nwakaemmanuelchimezie@gmail.com>
2020-10-26 14:36:31 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// var displayBlock = document.querySelector('#btn').addEventListener('click', displayUserProfile);
+// displayBlock = 1
+// document.querySelector('.info').style.display = 'none'; 
+// function displayUserProfile(){
+//     if(displayBlock = 1){
+//         document.querySelector('.info').style.display = 'block'; 
+//     }
+// }
